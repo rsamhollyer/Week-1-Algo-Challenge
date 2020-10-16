a0f9ec1fdfae85557019d782a3c178bbf3757b8f
Sam Hollyer <rsamhollyer@gmail.com>
2020-10-16 11:43:49 -0500

    Algos

---
+++ sum-multiples.py
@@ -0,0 +1,10 @@
+#2. Sum of All Multiples of 3 or 5 below 1000
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+numbers = range(1,1001)
+output = 0
+for i in numbers:
+    if i % 3 == 0 or i % 5 == 0:
+        output += i
+    print(output)
