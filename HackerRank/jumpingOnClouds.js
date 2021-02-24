f4de28b9765954ffaefb6f7a157ae7da6cd2efa6
Sam Hollyer <rsamhollyer@gmail.com>
2021-02-24 14:03:14 -0600

    updated

--- HackerRank/jumpingOnClouds.js
+++ HackerRank/jumpingOnClouds.js
@@ -1,7 +1,24 @@
 function jumpingOnClouds(c) {
-	index = 0;
-	jumps = 0;
-	while index >= c.length-1
+	//We always start on index 0 - implies looping
+	//We want to stop on the last 0 value - arr[arr.length-1]
+	// Along the way we count # of jumps
+	//Can jump by 1 or 2
+	//Cannot land on value of 1
+	let jumps = 0;
+	let i = 0;
+
+	while (i < c.length) {
+		//look at c[i+1] & c[i+2]
+		//prefer c[i+2] if not equal to 1
+		if (c[i + 2] !== 1) {
+			i = i + 2;
+		} else {
+			i++;
+		}
+		jumps++;
+	}
+
+	return jumps;
 }
 
-console.log(jumpingOnClouds([0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0]));
+console.log(jumpingOnClouds([0, 1, 0, 0, 0, 1, 0]));
